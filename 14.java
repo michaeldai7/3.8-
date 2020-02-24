b64774f4c3ced3d550d27fcc09e34bef85cfa777
michaeldai7 <michealxia7@gmail.com>
2020-02-24 18:25:29 -0500

    final bug fixed

--- 14.java
+++ 14.java
@@ -1,80 +1,53 @@
 import java.util.*;
 
 import javax.lang.model.util.ElementScanner6;
-class Forteen {
-  public static void main (String[] args) {
-  Scanner sc = new Scanner(System.in);
-  int quarters, dimes, nickels, cents;
-  int afterquarter, afterdimes, afternickels, aftercents;
 
-  System.out.println("Enter the number of cents you have.");
-  cents = sc.nextInt();
-  quarters = cents/25;
-  afterquarter = cents%25;
-  dimes = afterquarter/10;
-  afterdimes = afterquarter%10;
-  nickels = afterdimes/5;
-  afternickels = afterdimes%5;
-  aftercents = afternickels;
-  int[] coins = {quarters,dimes,nickels,aftercents};
-  int length = 0;
-  String answer = ("");
-  while(length < coins.length){
-	  if (length == 0 && quarters == 0){
-		  ;
-	  }else{
-		answer = answer + "quarter is " + quarters + ",";
-		if (length == 1 && dimes == 0){
-		  ;
-		}else{
-		  answer = answer + "dime is " + dimes + ",";
-		  if (length == 2 && nickels == 0){
-		  ;
-		}else{
-	  		answer = answer + "nickel is " + nickels + ",";
-			 if (length == 3 && aftercents == 0){
-		 ;
-	}else{
-	  answer = answer + "cents is " + aftercents + ".";
-	} 
-		}
-	
-	}
-	
-	}
-	  
-	length += 1;
-	
+class Forteen {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int quarters, dimes, nickels, cents;
+    int afterquarter, afterdimes, afternickels, aftercents;
+
+    System.out.println("Enter the number of cents you have.");
+    cents = sc.nextInt();
+    quarters = cents / 25;
+    afterquarter = cents % 25;
+    dimes = afterquarter / 10;
+    afterdimes = afterquarter % 10;
+    nickels = afterdimes / 5;
+    afternickels = afterdimes % 5;
+    aftercents = afternickels;
+    int[] coins = { quarters, dimes, nickels, aftercents };
+    int length = 0;
+    String answer = ("");
+    if (length == 0 && quarters == 0) {
+      ;
+      length += 1;
+    } else {
+      answer = answer + "quarter is " + quarters + ",";
+      length += 1;
+    }
+    if (length == 1 && dimes == 0) {
+      ;
+      length += 1;
+    } else {
+      answer = answer + "dime is " + dimes + ",";
+      length += 1;
+    }
+    if (length == 2 && nickels == 0) {
+      ;
+      length += 1;
+    } else {
+      answer = answer + "nickel is " + nickels + ",";
+      length += 1;
+    }
+    if (length == 3 && aftercents == 0) {
+      ;
+      length += 1;
+    } else {
+      answer = answer + "cents is " + aftercents + ".";
+      length += 1;
+    }
+    System.out.println(answer);
   }
-  System.out.println(answer);
-/*
-  if(quarters == 0 && dimes != 0 && nickels != 0 &&cents != 0)
-    System.out.println(cents +" cents: " + dimes + " dimes," + nickels + " nickels, " + aftercents + " cnets.");
-  else if (quarters != 0 && dimes == 0 && nickels != 0 &&cents != 0)
-    System.out.println(cents +" cents: " + quarters + " quarters," + nickels + " nickels, " + aftercents + " cnets.");
-  else if (quarters != 0 && dimes != 0 && nickels == 0 &&cents != 0)
-    System.out.println(cents +" cents: " + quarters + " quarters," + dimes + " dimes," + aftercents + " cnets.");
-  else if (quarters != 0 && dimes != 0 && nickels != 0 &&cents == 0)
-    System.out.println(cents +" cents: " + quarters + " quarters," + dimes + " dimes," + nickels + " nickels.");
-  else if (quarters == 0 && dimes == 0 && nickels != 0 &&cents != 0)
-    System.out.println(cents +" cents: " + nickels + " nickels, " + aftercents + " cents.");
-  else if (quarters != 0 && dimes == 0 && nickels == 0 &&cents != 0)
-    System.out.println(cents +" cents: " + quarters + " quarters, " + aftercents + " cents.");
-  else if (quarters != 0 && dimes != 0 && nickels == 0 &&cents == 0)
-    System.out.println(cents +" cents: " + quarters + " quarters, " + dimes + " dimes.");
-  else if (quarters == 0 && dimes != 0 && nickels != 0 &&cents == 0)
-    System.out.println(cents +" cents: " + dimes + " dimes, " + nickels + " nickels.");
-  else if (quarters == 0 && dimes == 0 && nickels == 0 &&cents != 0)
-    System.out.println(cents +" cents: " + aftercents + " cents.");
-  else if (quarters != 0 && dimes == 0 && nickels == 0 &&cents == 0)
-    System.out.println(cents +" cents: " + quarters + " quarters.");
-  else if (quarters == 0 && dimes != 0 && nickels == 0 &&cents == 0)
-    System.out.println(cents +" cents: " + dimes + " dimes.");
-  else if (quarters == 0 && dimes == 0 && nickels != 0 &&cents == 0)
-    System.out.println(cents +" cents: " + nickels + " nickels.");
-  else if (quarters == 0 && dimes == 0 && nickels == 0 &&cents == 0)
-    System.out.println(cents +" cents: " + " None.");
-  else 
-    System.out.println(cents +" cents: " + quarters + " quarters," + dimes + " dimes," + nickels + " nickels, " + aftercents + " cnets.");*/
-   }
 }
--- 15.java
+++ 15.java
@@ -1,27 +1,27 @@
 import java.util.*;
-class Fifteen{
-	public static void main(String[] args){
+
+class Fifteen {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		double income = sc.nextDouble(); 
+		double income = sc.nextDouble();
 		double houseCost = sc.nextDouble();
 		double totalChildren = sc.nextDouble();
 		double schoolChildren = sc.nextDouble();
 		double tax;
-		if (houseCost > 8000){
+		if (houseCost > 8000) {
 			tax = income * 0.18;
-		
-		
-		}else{
+
+		} else {
 			tax = (income - 10000) * 0.18;
 		}
-		if (houseCost < 6000 && totalChildren > 1 && schoolChildren > 0){
-			tax = tax - ((schoolChildren * 1000) + ((totalChildren - schoolChildren)*500)) * 0.18;
+		if (houseCost < 6000 && totalChildren > 2 && schoolChildren > 0) {
+			tax = tax - ((schoolChildren * 1000) + ((totalChildren - schoolChildren) * 500)) * 0.18;
 		}
-		if (tax > 2000){
+		if (tax > 2000) {
 			tax *= 1.15;
 		}
-		if (tax < 0){
-			tax =0;
+		if (tax < 0) {
+			tax = 0;
 		}
 		System.out.println(tax);
 	}
