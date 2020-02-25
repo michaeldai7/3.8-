2d37a086c95e6569cad6c1f0bf3f2e924a777395
michaeldai7 <michealxia7@gmail.com>
2020-02-25 19:51:57 +0000

    10

--- 10.java
+++ 10.java
@@ -1,24 +1,21 @@
 import java.util.*;
-
 class Ten {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the mass number of the letter");
-    int mass = sc.nextInt();
-    if (mass <= 30) {
-      System.out.println("The cost of mailing a letter is 40 sinas");
-    } else if (mass <= 50) {
-      System.out.println("The cost of mailing a letter is 55 sinas");
-    } else if (mass <= 100) {
-      System.out.println("The cost of mailing a letter is 70 sinas");
-    } else if (mass > 100) {
-      mass = mass - 100;
-      if (mass < 50) {
-        System.out.println("The cost of mailing a letter is 70 sinas");
-      } else if (mass >= 50) {
-        mass = mass / 50 * 25 + 70;
-        System.out.println("The cost of mailing a letter is " + mass + " sinas.");
-      }
-    }
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter the mass number of the letter");
+  double mass = sc.nextDouble();
+  if (mass <= 30) {
+    System.out.println("The cost of mailing a letter is 40 sinas");
+  }
+  else if(mass <= 50) {
+    System.out.println("The cost of mailing a letter is 55 sinas");
+  }
+  else if(mass <= 100) {
+    System.out.println("The cost of mailing a letter is 70 sinas");
+  }
+  else {
+    cost = ((((int) (mass - 100) / 50) * 25)) + 70;
+    System.out.println("The cost of mailing a letter is " + cost + " sinas");
+  }
   }
 }
